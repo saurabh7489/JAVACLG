59ae1b8dc21fc34d162a3f71f16dfcaa64083a01
saurabh7489 <saurabhpandey7489@gmail.com>
2026-04-01 12:50:28 +0530

    initial commit

---
+++ lec1.java
@@ -0,0 +1,84 @@
+// import java.util.Scanner;
+
+// public class class1 {
+//     public static int add(int a,int b){
+//         return a+b;
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+//         int sum = add(a, b);
+//         System.out.println(sum);
+//     }
+
+    
+// }
+// import java.util.Scanner;
+
+// public class lec1 {
+//     public static int freq(int a,int b)
+//     {
+//         int count=0;
+//         while(a>0){
+// int rem=a%10;
+// if (rem==b) 
+//     count++;
+//     a=a/10;
+    
+//         }
+//         return count;
+    
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int a= sc.nextInt();
+//         int b=sc.nextInt();
+//         int count=freq(a, b);
+//         System.out.println(count);
+
+//     }
+// }
+// import java.util.Scanner;
+
+// public class class1 {
+// public static int binary(int a){
+//   while (a>0) {
+//     int rem
+//   }
+// }
+    
+
+//     public static void main(String[]args){
+// Scanner sc=new Scanner(System.in);
+// int a=sc.nextInt();
+// int convert=binary(a);
+// System.out.println(convert);
+//     }
+// }
+
+// import java.util.Scanner;
+
+// public class lec1 {
+
+//   public static void main(String[] args) {
+//     Scanner sc=new Scanner(System.in);
+//     int a=sc.nextInt();
+//     int[] arr=new int[a];
+//     for(int i=0;i<a;i++){
+//       arr[i]=sc.nextInt();
+//     }
+//     for(int i=0;i<a;i++){
+//       System.out.print(arr[i]+"");
+//     }
+//   }
+// }
+
+import java.util.Scanner;
+
+public class lec1 {
+
+    public static void main(String[] args) {
+       
+    }
+}
