ec4408ec3f99bec2fb04b47f0e857f75ae863292
saurabh7489 <saurabhpandey7489@gmail.com>
2026-04-06 11:26:25 +0530

    initial commit

---
+++ spiral.java
@@ -0,0 +1,18 @@
+public class spiral {
+    public static void main(String[] args) {
+        
+        int=0;
+        int bottom=arr.length-1;
+        int left =0;
+        int right = arr [0].length-1;
+
+        while(top<=bottom && left <= right)
+        {
+            for(int i=left; i<=right;i++)
+            {
+                System.out.print(arr[top][i]+"");
+            }
+            top**;
+        }
+    }
+}
