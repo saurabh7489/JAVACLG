28fed0daac76a8d31b981994203cd595b1a4d5f3
saurabh7489 <saurabhpandey7489@gmail.com>
2026-04-24 10:19:23 +0530

    initial commit

---
+++ dayeight.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class dayeight {
+    static int fibo(int n,int[] dp){
+        if(n<=1){
+            return n;
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        dp[n]=fibo(n-1,dp)+fibo(n-2,dp);
+        return dp[n];
+    }
+    public static void main(String[] args) {
+        int n=5;
+        int[] dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        System.out.println(fibo(n, dp));
+    }
+}
